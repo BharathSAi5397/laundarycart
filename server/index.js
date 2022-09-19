71f84318f7bfe26ceec60e3fffa8a39d85235916
BharathSAi5397 <sai.saibharath13@gmail.com>
2022-09-19 21:28:32 +0530

    model commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser=require("body-parser");
+const mongoose = require("mongoose");
+const dummyRoutes= require("./routes/dummyroutes")
+
+
+//middleware
+app.use(bodyParser())
+
+//mongoose connection
+main().catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect(
+      "mongodb+srv://10xlaundary:laundary1234@cluster0.on6dtr0.mongodb.net/laundarycollection?retryWrites=true&w=majority"
+    );
+    console.log("Database Connected");
+  }
+
+//dummy routes
+app.use(dummyRoutes);
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("ok");
