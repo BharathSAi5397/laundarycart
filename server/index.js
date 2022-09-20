6d384080b4aa8e6f84c8cb4d5a28c64ecb4d6d89
BharathSAi5397 <sai.saibharath13@gmail.com>
2022-09-20 15:13:23 +0530

    route get order

--- server/index.js
+++ server/index.js
@@ -1,34 +1,38 @@
 const express = require("express");
 const app = express();
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
+const {UserData,OrdersData}= require("./models/laundarymodel");
 const mongoose = require("mongoose");
-const dummyRoutes= require("./routes/dummyroutes")
+const dummyRoutes = require("./routes/dummyroutes");
+const { reqAuth } = require("./middlewares/authmiddleware");
+const  orderRoutes  = require("./routes/orderRoutes");
 
 
 //middleware
-app.use(bodyParser())
+app.use(bodyParser());
+
+//app.use("/api/v1/order",reqAuth);//authentication middleware
 
 //mongoose connection
 main().catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect(
-      "mongodb+srv://10xlaundary:laundary1234@cluster0.on6dtr0.mongodb.net/laundarycollection?retryWrites=true&w=majority"
-    );
-    console.log("Database Connected");
-  }
+  await mongoose.connect(
+    "mongodb+srv://10xlaundary:laundary1234@cluster0.on6dtr0.mongodb.net/laundarycollection?retryWrites=true&w=majority"
+  );
+  console.log("Database Connected");
+}
 
 //dummy routes
-app.use(dummyRoutes);
+//app.use(dummyRoutes);
+//final get orders route
+app.use("/api/v1",orderRoutes);
 
-
-
-
-app.get("/",(req,res)=>{
-    res.send("ok");
+app.get("/", (req, res) => {
+  res.send("ok");
 })
 
 const port = process.env.PORT || 8080;
-app.listen(port,()=>{
-    console.log(`listening on ${port}`)
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
 })
